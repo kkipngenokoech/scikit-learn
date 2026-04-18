26dbfff11c6634f3729ac97035d36486a2e348fc
kipngeno koech <kkipngenokoech22@gmail.com>
2026-04-18 19:19:59 +0200

    feat: expose warm_start parameter in IsolationForest

--- sklearn/ensemble/iforest.py
+++ sklearn/ensemble/iforest.py
@@ -91,6 +91,11 @@ class IsolationForest(BaseBagging, OutlierMixin):
         ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
         for more details.
 
+    warm_start : bool, optional (default=False)
+        When set to ``True``, reuse the solution of the previous call to fit
+        and add more estimators to the ensemble, otherwise, just fit a whole
+        new forest. See :term:`the Glossary <warm_start>`.
+
     behaviour : str, default='old'
         Behaviour of the ``decision_function`` which can be either 'old' or
         'new'. Passing ``behaviour='new'`` makes the ``decision_function``
@@ -173,7 +178,8 @@ class IsolationForest(BaseBagging, OutlierMixin):
                  n_jobs=None,
                  behaviour='old',
                  random_state=None,
-                 verbose=0):
+                 verbose=0,
+                 warm_start=False):
         super().__init__(
             base_estimator=ExtraTreeRegressor(
                 max_features=1,
@@ -185,6 +191,7 @@ class IsolationForest(BaseBagging, OutlierMixin):
             n_estimators=n_estimators,
             max_samples=max_samples,
             max_features=max_features,
+            warm_start=warm_start,
             n_jobs=n_jobs,
             random_state=random_state,
             verbose=verbose)
